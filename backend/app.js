9132c0e2a15a975357e160242c07f09fb7e94767
santosh <santhosh.pujari@brayleinosplash.com>
2025-09-25 10:40:35 +0530

    my order functionality

--- backend/app.js
+++ backend/app.js
@@ -23,39 +23,32 @@ app.get('/meals', async (req, res) => {
 app.post('/orders', async (req, res) => {
   const orderData = req.body.order;
 
-  if (orderData === null || orderData.items === null || orderData.items.length === 0) {
-    return res
-      .status(400)
-      .json({ message: 'Missing data.' });
+  if (!orderData || !orderData.customer) {
+    return res.status(400).json({ message: 'Missing customer data.' });
   }
 
-  if (
-    orderData.customer.email === null ||
-    !orderData.customer.email.includes('@') ||
-    orderData.customer.name === null ||
-    orderData.customer.name.trim() === '' ||
-    orderData.customer.street === null ||
-    orderData.customer.street.trim() === '' ||
-    orderData.customer['postal-code'] === null ||
-    orderData.customer['postal-code'].trim() === '' ||
-    orderData.customer.city === null ||
-    orderData.customer.city.trim() === ''
-  ) {
-    return res.status(400).json({
-      message:
-        'Missing data: Email, name, street, postal code or city is missing.',
-    });
+  const { name, email, street, code, city } = orderData.customer;
+
+  if (!name || !email.includes('@') || !street || !code || !city) {
+    return res.status(400).json({ message: 'All fields are required.' });
   }
 
   const newOrder = {
     ...orderData,
     id: (Math.random() * 1000).toString(),
   };
+
   const orders = await fs.readFile('./data/orders.json', 'utf8');
   const allOrders = JSON.parse(orders);
   allOrders.push(newOrder);
   await fs.writeFile('./data/orders.json', JSON.stringify(allOrders));
-  res.status(201).json({ message: 'Order created!' });
+
+  res.status(201).json({ message: 'Form submitted!' });
+});
+
+app.get('/orders', async (req, res) => {
+  const orders = await fs.readFile('./data/orders.json', 'utf8');
+  res.json(JSON.parse(orders));
 });
 
 app.use((req, res) => {
